b6eaf4dedd103693eac429d38c3bd546dda2c25e
johanwulf98 <johanwulf98@gmail.com>
2023-11-21 09:56:38 +0100

    feat: add color pickers

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,29 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import { getDates } from "./utils/calendar";
 
+type CommitDay = {
+  date: Date;
+  color: string;
+};
+
 function App() {
   const [year, setYear] = useState(2023);
-  const [days, setDays] = useState<Date[]>([]);
+  const [days, setDays] = useState<CommitDay[]>([]);
+  const [selectedColor, setSelectedColor] = useState("#39d353");
+  const [painterSize, setPainterSize] = useState("small");
 
   useEffect(() => {
-    setDays(getDates(year));
+    const days: CommitDay[] = [];
+    const dates = getDates(year);
+    dates.forEach((date) => {
+      days.push({
+        date,
+        color: "#21262d",
+      });
+    });
+    setDays(days);
   }, [year]);
 
-  console.log("rerender");
   return (
     <>
       <select
@@ -23,20 +37,48 @@ function App() {
         <option value="2022">2022</option>
         <option value="2023">2023</option>
       </select>
-      <div className="text-3xl font-bold">Hello world</div>
+      <select
+        id="color"
+        name="color"
+        onChange={(e) => setSelectedColor(e.target.value)}
+      >
+        <option value="#39d353">lightest</option>
+        <option value="#26a641">second lightest</option>
+        <option value="#006d32">second darkest</option>
+        <option value="#0e4429">darkest</option>
+      </select>
+      <select
+        id="painter-size"
+        name="painter-size"
+        onChange={(e) => setPainterSize(e.target.value)}
+      >
+        <option value="small">Small</option>
+        <option value="medium">Medium</option>
+        <option value="large">Large</option>
+      </select>
       <div className="calendar-grid">
         {[...Array(Math.ceil(days.length / 7))].map((_, weekIndex) => (
           <div key={weekIndex} className="calendar-row">
             {[...Array(7)].map((_, dayIndex) => {
               const dateIndex = weekIndex * 7 + dayIndex;
+              // get date from commitday object
               const date = days[dateIndex];
 
-              if (date && date.getFullYear() === year) {
-                return <p key={dateIndex} className="calendar-date"></p>;
-              } else {
+              if (date && date.date.getFullYear() === year) {
                 return (
-                  <p key={dateIndex} className="calendar-date outside"></p>
+                  <p
+                    key={dateIndex}
+                    className={`calendar-date ${date.date}`}
+                    style={{ background: date.color }}
+                    onMouseDown={(e) => {
+                      const updatedDate = days[dateIndex];
+                      updatedDate.color = selectedColor;
+                      setDays([...days, updatedDate]);
+                    }}
+                  ></p>
                 );
+              } else {
+                return <></>;
               }
             })}
           </div>
