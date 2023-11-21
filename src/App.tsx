3cffd62724c9b727d4b16283cf9db0da6702d9e9
johanwulf98 <johanwulf98@gmail.com>
2023-11-21 10:54:32 +0100

    feat: add export

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,6 @@ function App() {
   const [year, setYear] = useState(2023);
   const [days, setDays] = useState<CommitDay[]>([]);
   const [selectedColor, setSelectedColor] = useState("#39d353");
-  const [painterSize, setPainterSize] = useState("small");
 
   useEffect(() => {
     const days: CommitDay[] = [];
@@ -24,6 +23,47 @@ function App() {
     });
     setDays(days);
   }, [year]);
+
+  const handleExport = () => {
+    const arr = [];
+    for (const day in days) {
+      let commitcount = 0;
+      if (days[day].color === "#39d353") {
+        commitcount = 20;
+      } else if (days[day].color === "#26a641") {
+        commitcount = 10;
+      } else if (days[day].color === "#006d32") {
+        commitcount = 5;
+      } else if (days[day].color === "#0e4429") {
+        commitcount = 1;
+      } else {
+      }
+      if (commitcount > 0) {
+        arr.push({
+          date: days[day].date,
+          commitcount,
+        });
+      }
+    }
+
+    console.log(arr);
+    exportToJson(arr);
+  };
+  const exportToJson = (objectData: any) => {
+    let filename = "export.json";
+    let contentType = "application/json;charset=utf-8;";
+    var a = document.createElement("a");
+    a.download = filename;
+    a.href =
+      "data:" +
+      contentType +
+      "," +
+      encodeURIComponent(JSON.stringify(objectData));
+    a.target = "_blank";
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+  };
 
   return (
     <>
@@ -47,15 +87,22 @@ function App() {
         <option value="#006d32">second darkest</option>
         <option value="#0e4429">darkest</option>
       </select>
-      <select
-        id="painter-size"
-        name="painter-size"
-        onChange={(e) => setPainterSize(e.target.value)}
+      <button
+        onClick={() => {
+          const days: CommitDay[] = [];
+          const dates = getDates(year);
+          dates.forEach((date) => {
+            days.push({
+              date,
+              color: "#21262d",
+            });
+          });
+          setDays(days);
+        }}
       >
-        <option value="small">Small</option>
-        <option value="medium">Medium</option>
-        <option value="large">Large</option>
-      </select>
+        Reset
+      </button>
+      <button onClick={handleExport}>Export</button>
       <div className="calendar-grid">
         {[...Array(Math.ceil(days.length / 7))].map((_, weekIndex) => (
           <div key={weekIndex} className="calendar-row">
